ec6bfb3563e59c12f2bc06e6b84ccf6c7c9e26d1
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-09-30 13:39:11 +0100

    Initial commit

---
+++ create.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eux
+
+ns="${1}"
+namespace="--namespace=${ns}"
+
+kubectl create namespace "${ns}"
+kubectl create "${namespace}" --filename="pods.yaml"
+
+until [ "3" -eq "$(kubectl get pods "${namespace}" | grep -c Running)" ] ; do sleep 1 ; done
+
+kubectl_exec() {
+  pod="$1" ; cmd="$2" ; shift 2 ; args=("$@")
+  kubectl exec "${namespace}" --stdin --tty "${pod}" "${cmd}" -- "${args[@]}"
+}
+
+for n in "master-00" "node-01" "node-02" ; do
+  kubectl_exec "${n}" systemctl start kubelet docker
+done
+
+token="--token=d9e800.955fc7f8f87970e6"
+
+kubectl_exec "master-00" kubeadm init "${token}"
+kubectl_exec "master-00" kubectl get nodes --output=wide
+master_ip="$(kubectl get pod "${namespace}" master-00 --output="go-template={{.status.podIP}}")"
+
+for n in "node-01" "node-02" ; do
+  kubectl_exec "${n}" kubeadm join "${token}" "${master_ip}"
+done
